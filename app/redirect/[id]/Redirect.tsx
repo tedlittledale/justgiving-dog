4a007a4e42a8e2ca012de361af9208578837ebbd
tedlittledale <ted.littledale@gmail.com>
2024-06-04 11:36:54 +0100

    remove direct

--- app/redirect/[id]/Redirect.tsx
+++ app/redirect/[id]/Redirect.tsx
@@ -5,9 +5,9 @@ import { navigate } from "@/app/actions";
 
 const Redirect = ({ url }: { url: string }) => {
   useEffect(() => {
-    setTimeout(() => {
-      navigate(url);
-    }, 100);
+    // setTimeout(() => {
+    //   navigate(url);
+    // }, 100);
   }, [url]);
 
   return null; // This component doesn't render anything
