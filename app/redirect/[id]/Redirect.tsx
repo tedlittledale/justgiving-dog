5f476597cc786c63a31faa27be0a39a1afb2e147
tedlittledale <ted.littledale@gmail.com>
2024-06-04 11:42:55 +0100

    client side redirect

--- app/redirect/[id]/Redirect.tsx
+++ app/redirect/[id]/Redirect.tsx
@@ -1,13 +1,14 @@
 "use client";
 import React, { useEffect } from "react";
 
-import { navigate } from "@/app/actions";
+import { useRouter } from "next/navigation";
 
 const Redirect = ({ url }: { url: string }) => {
+  const router = useRouter();
   useEffect(() => {
-    // setTimeout(() => {
-    //   navigate(url);
-    // }, 100);
+    setTimeout(() => {
+      router.push(url);
+    }, 100);
   }, [url]);
 
   return null; // This component doesn't render anything
